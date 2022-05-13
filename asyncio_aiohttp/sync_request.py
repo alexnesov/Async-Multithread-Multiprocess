f710b2e7128f3db98e578af6a57939bcba1eacb2
alexnesov <alexandrenesovic@gmail.com>
2022-05-13 15:12:00 +0200

    clean

--- asyncio_aiohttp/sync_request.py
+++ asyncio_aiohttp/sync_request.py
@@ -4,12 +4,7 @@ import time
 
 
 api_key = os.getenv("alpha_vantage_api_key")
-
-print(api_key)
-
-api_key = os.getenv('ALPHAVANTAGE_API_KEY')
-
-url = 'https://www.alphavantage.co/query?function=OVERVIEW&symbol={}&apikey={}'
+url     = 'https://www.alphavantage.co/query?function=OVERVIEW&symbol={}&apikey={}'
 
 symbols = ['AAPL', 'GOOG', 'TSLA', 'MSFT', 'PEP']
 
